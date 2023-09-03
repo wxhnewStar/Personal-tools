d40b24f2b1d1c64e333026341da459c5856cad0c
shainvol <wxh_newStar@foxmail.com>
2023-09-03 22:31:14 +0800

    add: recognize the format of "![]()", and fix these fail reference. In addition,change the '\' to '/'.

--- ImageRefUnifier/ImageRefUnifier.py
+++ ImageRefUnifier/ImageRefUnifier.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import re
 
 
+# function to get the path to the folder containing the images to be unified
 def getUserInput():
     print("Please enter the path to the folder containing the images to be unified:")
     path = input()
@@ -31,7 +33,13 @@ class FileOrFolder:
             for child in self.children:
                 child.print_structure(indent + 1)
 
+    def hierachyFunc(self, func):
+        func(self)
+        for child in self.children:
+            child.hierachyFunc(func)
 
+
+# build file tree based on given path
 def buildFileTree(path):
     if not os.path.isdir(path):
         return FileOrFolder(path, False)
@@ -44,7 +52,84 @@ def buildFileTree(path):
     return root_path
 
 
+# judge whehter the given item is markdown file
+def isMarkdownFile(cur_item):
+    if (cur_item.is_dir or (".md" not in cur_item.name)):
+        return False
+    return True
+
+
+# show the way of image reference in current file
+def showImageReferenceWay(cur_item):
+    if not isMarkdownFile(cur_item):
+        return
+
+    file = open(cur_item.name, "r")
+    if (file == None):
+        print("open file failed")
+    content = file.read()
+    file.close()
+
+    # find all image reference
+    pattern = re.compile(r'!\[.*?]\(.*?\)')
+    image_refs = pattern.findall(content)
+
+    # show the way of image reference
+    for imageRef in image_refs:
+        print(imageRef)
+
+
+# calculate the level of directory
+def directoryLevel(file_name):
+    # print(f"file name is {file_name}")
+    notebook_top_level = file_name.find("Typora files")
+    length = len(file_name) - 1
+    index = notebook_top_level + len("Typora files") + 1
+    # print(f"current index is {index}, {file_name[index]}, {file_name[index+1]}")
+    directory_levels = 0
+    while index != length:
+        if file_name[index] == "/":
+            directory_levels += 1
+        index += 1
+    return directory_levels
+
+
+# fix each line of image reference
+def fixLine(line, file_name):
+    if "http" in line:
+        return line
+
+    fix_line = line.replace("\\", "/")
+    if "../" in fix_line:
+        return fix_line
+    if "图库" in fix_line:
+        pre_fix = "../" * directoryLevel(file_name)
+        return re.sub(r"\(.*Typora files/", "(" + pre_fix, fix_line)
+    return fix_line
+
+
+#  check and fix the reference of image for given file
+def fixImageReference(cur_item):
+    if not isMarkdownFile(cur_item):
+        return
+
+    pattern = re.compile(r'!\[.*?]\(.*?\)')
+    write_lines = []
+    with open(cur_item.name, "r") as file:
+        for line in file:
+            if pattern.match(line):
+                new_line = fixLine(line, cur_item.name)
+                # print(f"previous line is [{line}],and new line is [{new_line}] ")
+                write_lines.append(new_line)
+            else:
+                write_lines.append(line)
+    with open(cur_item.name, "w") as file:
+        file.writelines(write_lines)
+
+
 if __name__ == '__main__':
     unifiedPath = getUserInput()
     fileTree = buildFileTree(unifiedPath)
-    fileTree.print_structure()
+    # fileTree.print_structure()
+    # fileTree.hierachyFunc(showImageReferenceWay)
+    fileTree.hierachyFunc(fixImageReference)
